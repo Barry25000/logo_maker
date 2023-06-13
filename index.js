04e68ff945001df63b3752476629e6a78d27cf24
Barry25000 <barryengler@gmail.com>
2023-06-12 23:47:17 -0500

    bug fix

--- index.js
+++ index.js
@@ -12,32 +12,32 @@ function writeToFile(fileName, answers) {
     
     svgString = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">' //sets width and height of container
     
-    svgString += '<g>'; // <g> element is used to group svg layers into one layer 
+    svgString += '<g>'; // <g> element is used to group svg layers into one layer, being added to the svgString
 
-    svgString += '${answers.shape}'; //user input and inserts into svg file
+    svgString += '${answers.shape}'; //user input, checking for shape below as well as being added to the svgString
 
     let shapeChoice; // checking inputed shape and running conditional tests
     if (answers.shape === 'Circle') {
        shapeChoice = new Circle(); 
-       svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackgroundColor}"/>`;
+       svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeColor}"/>`;
     } else if (answers.shape === 'Square') {
         shapeChoice = new Square();
-        svgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeBackgroundColor}"/>`;      
+        svgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeColor}"/>`;      
     }else{
         shapeChoice = new Triangle();
-        svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`;
+        svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeColor}"/>`;
     }
-        // setting text allignment and font size
+        // setting text allignment and font size as well as terminating </g> and </svg>
     svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
     svgString += '</g>'; 
-    svgString += "</svg>";
+    svgString += '</svg>';
 
     fs.writeFile(fileName, svgString, (err) => { // writting file as well as running error check
-        err ? console.log(err) : console.log("Generated logo.svg");
+        err ? console.log(err) : console.log('Generated logo.svg');
       });
 }
 
-function userQuestions () {
+function userQuestions () { // begin user questions for logo
     inquirer
         .prompt([
             {
@@ -60,11 +60,11 @@ function userQuestions () {
                 type: 'input',
                 message: 'What color do you want the shape to be? (Enter color or a hexadecimal color number)',
                 choices: ['circle', 'square', 'triangle'],
-                name: 'shapeBackgroundColor',
+                name: 'shapeColor',
             }
         ])
         .then((answers) => {
-            if (answers.text.length > 3) {
+            if (answers.text.length > 3) { // check text length <=3, prompt if greater than 3, otherwise write to file
               console.log('Must enter a value of no more than 3 characters');
             } else {
               writeToFile('logo.svg', answers);
@@ -72,4 +72,4 @@ function userQuestions () {
         });
 }
 
-userQuestions(); // user questions start when ran
+userQuestions(); // user questions (inquirer) starts on load
