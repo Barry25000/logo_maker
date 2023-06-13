579fea7cfe4e6d02326ca73058133718e2c644cb
Barry25000 <barryengler@gmail.com>
2023-06-13 16:26:27 -0500

    Display issues

--- index.js
+++ index.js
@@ -3,29 +3,7 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const { Circle, Square, Triangle } = require("./lib/shapes.js");
 
-function writeToFile(fileName, answers) {
-  let svgString = ""; //sets sting to null
-
-  svgString = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><g>${answers.shape}`;
-  let shapeChoice; // checking inputed shape and running conditional tests
-  if (answers.shape === "Circle") {
-    shapeChoice = new Circle();
-    svgString += `<circle cx="150" cy="100" r="80" fill="${answers.shapeColor}"/><text x="150" y="125" text-anchor="middle" font-size="60" fill="${answers.textColor}">${answers.text}</text></g></svg>`;
-  } else if (answers.shape === "Square") {
-    shapeChoice = new Square();
-    svgString += `<rect x="90" y="40" width="120" height="120" fill="${answers.shapeColor}"/><text x="150" y="125" text-anchor="middle" font-size="60" fill="${answers.textColor}">${answers.text}</text></g></svg>`;
-  } else {
-    shapeChoice = new Triangle();
-    svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeColor}"/><text x="150" y="150" text-anchor="middle" font-size="60" fill="${answers.textColor}">${answers.text}</text></g></svg>`;
-  }
-
-  fs.writeFile(fileName, svgString, (err) => {
-    // writting file as well as logging error
-    err ? console.log(err) : console.log("Generated logo.svg");
-  });
-}
-
-// begin design questions for logo
+// design questions for logo
 function questions() {
   inquirer
     .prompt([
@@ -63,6 +41,28 @@ function questions() {
         writeToFile("logo.svg", answers);
       }
     });
+}
+
+function writeToFile(fileName, answers) {
+  let svgString = ""; //sets sting to null
+
+  svgString = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><g>${answers.shape}`;
+  let shapeChoice; // checking inputed shape and running conditional tests
+  if (answers.shape === "Circle") {
+    shapeChoice = new Circle();
+    svgString += `<circle cx="150" cy="100" r="80" fill="${answers.shapeColor}"/><text x="150" y="125" text-anchor="middle" font-size="60" fill="${answers.textColor}">${answers.text}</text></g></svg>`;
+  } else if (answers.shape === "Square") {
+    shapeChoice = new Square();
+    svgString += `<rect x="90" y="40" width="120" height="120" fill="${answers.shapeColor}"/><text x="150" y="125" text-anchor="middle" font-size="60" fill="${answers.textColor}">${answers.text}</text></g></svg>`;
+  } else {
+    shapeChoice = new Triangle();
+    svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeColor}"/><text x="150" y="150" text-anchor="middle" font-size="60" fill="${answers.textColor}">${answers.text}</text></g></svg>`;
+  }
+
+  fs.writeFile(fileName, svgString, (err) => {
+    // writting file as well as logging error
+    err ? console.log(err) : console.log("Generated logo.svg");
+  });
 }
 
 questions(); // questions (inquirer) starts on load
